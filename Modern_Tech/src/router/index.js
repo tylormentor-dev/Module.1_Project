87f8e6c1d6b9a810d969da743b166e5b6072a061
amol3ng <amohelamokhele@gmail.com>
2025-12-10 09:45:22 +0200

    Payslip and Payroll calculator just added (#1)

--- Modern_Tech/src/router/index.js
+++ Modern_Tech/src/router/index.js
@@ -1,23 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login.vue'
+import Payroll from '../views/Payroll.vue'
+import Payslip from '../views/Payslip.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'login',
+      component: Login,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/payroll',
+      name: 'payroll',
+      component: Payroll,
+    },
+    { 
+      path: "/payslip", 
+      name: "payslip", 
+      component: Payslip,
     },
   ],
 })
+
 
 export default router
